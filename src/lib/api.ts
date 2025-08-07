0a0582ed7d6b4730293279711626a301259fe719
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-08-07 03:00:31 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const api = axios.create({
   // ATENÇÃO: Substitua pela URL real do seu backend.
   // Se o seu backend roda na porta 3001, por exemplo, seria 'http://localhost:3001/api'
-  baseURL: 'http://localhost:3000/api/v1', 
+  baseURL: 'http://168.231.95.166:3006/api/v1', 
   headers: {
     'Content-Type': 'application/json',
   },
